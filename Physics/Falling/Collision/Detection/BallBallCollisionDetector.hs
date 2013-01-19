5a016333a2327bffad035a8de10a712680a82255
Sébastien Crozet <developer@crozet.re>
2013-01-19 17:00:33 +0100

    Fix collision detectors.

--- Physics/Falling/Collision/Detection/BallBallCollisionDetector.hs
+++ Physics/Falling/Collision/Detection/BallBallCollisionDetector.hs
@@ -1,28 +1,31 @@
-module Physics.Falling.Collision.BallBallCollisionDetector
+module Physics.Falling.Collision.Detection.BallBallCollisionDetector
+(
+collideBallBall
+)
 where
 
-import Data.Vect.Double.Base
-import Physics.Falling.Shape.ExtraTransform
+import Data.Vect.Double.Base hiding(translation)
+import Physics.Falling.Math.Transform
 import Physics.Falling.Shape.Ball
 import Physics.Falling.Collision.Collision
 
 -- generic ballball collision detection algorithm
-collideBallBall :: (Position m v, UnitVector v n) =>
-                   Ball -> Ball -> m -> m -> Maybe (GeometricCollisionDescr v n)
+collideBallBall :: (Translation m v, UnitVector v n) =>
+                   Ball -> Ball -> m -> m -> Maybe (CollisionDescr v n)
 collideBallBall (Ball r1) (Ball r2) transform1 transform2 =
                                             if sqDist <= (r1 + r2) ** 2.0 then
-                                              Just $ GeometricCollisionDescr
-                                                     collisionCenter
-                                                     collisionNormal
-                                                     depth
+                                              Just $ CollisionDescr collisionCenter
+                                                                    collisionNormal
+                                                                    depth
                                             else
                                               Nothing
 
                                             where
-                                            center1         = position transform1
-                                            center2         = position transform2
-                                            deltaPos        = center1 &- center2
+                                            center1         = translation transform1
+                                            center2         = translation transform2
+                                            deltaPos        = center2 &- center1
                                             collisionNormal = mkNormal deltaPos
-                                            collisionCenter = center1 &+ center2 &* 0.5
+                                            nvect           = fromNormal collisionNormal
+                                            collisionCenter = (nvect &* (r1 - r2) &+ center1 &+ center2) &* 0.5
                                             sqDist          = normsqr deltaPos
-                                            depth           = -(sqrt sqDist - r1 - r2)
+                                            depth           = r1 + r2 - sqrt sqDist 
--- Physics/Falling/Collision/Detection/PlaneImplicitShapeCollisionDetector.hs
+++ Physics/Falling/Collision/Detection/PlaneImplicitShapeCollisionDetector.hs
@@ -1,38 +1,34 @@
-module Physics.Falling.Collision.PlaneImplicitShapeCollisionDetector
+module Physics.Falling.Collision.Detection.PlaneImplicitShapeCollisionDetector
 (
 collidePlaneImplicitShape
 )
 where
 
 
-import Data.Vect.Double.Base
-import Physics.Falling.Shape.ExtraTransform
+import Data.Vect.Double.Base hiding(translation)
+import Physics.Falling.Math.Transform
 import Physics.Falling.Shape.ImplicitShape
 import Physics.Falling.Shape.Plane
 import Physics.Falling.Collision.Collision
 
-collidePlaneImplicitShape :: (ImplicitShape g v,
-                              DeltaTransform m v,
-                              DeltaTransform mp v,
-                              Position mp v,
-                              AbelianGroup n,
-                              LeftModule m v,
-                              LeftModule mp v,
-                              UnitVector v n) =>
-                              Plane v -> g -> mp -> m -> m -> Maybe (GeometricCollisionDescr v n)
+collidePlaneImplicitShape :: (ImplicitShape  g v
+                              , Transform      m v av
+                              , Transform      mp v av
+                              , UnitVector     v n) =>
+                              Plane v -> g -> mp -> m -> m -> Maybe (CollisionDescr v n)
 collidePlaneImplicitShape (Plane upVec) other planeTransform otherTransform inverseOtherTransform =
-                             if d > 0.0 then
-                               Just $ GeometricCollisionDescr c n d
-                             else
-                               Nothing
-                             where
-                             planeCenter    = position planeTransform
-                             planeNormal    = mkNormal $ planeTransform `deltaTransform` upVec
-                             invPlaneNormal = neg planeNormal
-                             deepestPoint   = supportPointWithTransform other
-                                                                        invPlaneNormal
-                                                                        otherTransform
-                                                                        inverseOtherTransform
-                             n              = planeNormal
-                             c              = (deepestPoint &+ planeCenter)  &* 0.5
-                             d              = (planeCenter  &- deepestPoint) &. fromNormal planeNormal
+                          if d > 0.0 then
+                            Just $ CollisionDescr c n d
+                          else
+                            Nothing
+                          where
+                          planeCenter    = translation planeTransform
+                          planeNormal    = mkNormal $ planeTransform `deltaTransform` upVec
+                          invPlaneNormal = toNormalUnsafe $ neg $ fromNormal planeNormal
+                          deepestPoint   = supportPointWithTransform other
+                                                                     invPlaneNormal
+                                                                     otherTransform
+                                                                     inverseOtherTransform
+                          n              = planeNormal
+                          c              = (deepestPoint &+ fromNormal planeNormal &* (d * 0.5))
+                          d              = (planeCenter  &- deepestPoint) &. fromNormal planeNormal
