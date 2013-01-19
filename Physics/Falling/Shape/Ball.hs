2f24e17430b04092fd520b80f5709ed302df6126
Sébastien Crozet <developer@crozet.re>
2013-01-19 16:59:21 +0100

    Add implicit shape instance for balls.

--- Physics/Falling/Shape/Ball.hs
+++ Physics/Falling/Shape/Ball.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances     #-}
 
 module Physics.Falling.Shape.Ball
 (
@@ -9,8 +10,12 @@ Ball(..)
 where
 
 import Data.Vect.Double.Base
+import Physics.Falling.Shape.ImplicitShape
 
 newtype Ball = Ball Double
+
+instance (Vector v) => ImplicitShape Ball v where
+  supportPoint = ballSupportPoint
 
 ballSupportPoint :: (Vector v) => v -> Ball -> v
 ballSupportPoint direction (Ball radius) = direction &* radius
