9a923cc7d3224142a8892b4eba4eb4ae1ef988ec
Sébastien Crozet <developer@crozet.re>
2013-01-21 20:59:22 +0100

    Add GJK distance algorithm.

---
+++ Physics/Falling/Collision/Detection/GJK.hs
@@ -0,0 +1,64 @@
+module Physics.Falling.Collision.Detection.GJK
+(
+distance
+)
+where
+
+import Data.Vect.Double.Base hiding(translation, distance)
+import Physics.Falling.Math.Transform
+import Physics.Falling.Shape.ImplicitShape
+import Physics.Falling.Collision.Detection.Simplex hiding(dimension)
+import qualified Physics.Falling.Collision.Detection.Simplex as S (dimension)
+
+type SimplexResult v = Either (v, [ Double ], Simplex v, Simplex v) -- need more iterations
+                              (v, [ Double ], Simplex v)            -- success
+
+distance :: (ImplicitShape g1 v, ImplicitShape g2 v, Transform m v av, UnitVector v n, Eq v, Fractional v) =>
+            (g1, m, m) -> (g2, m, m) -> Int -> Double
+distance s1 s2 dimension = case algorithmGJK s1 s2 dimension $ Left (initialPoint, [], emptySimplex, emptySimplex) of
+                           Nothing                 -> 0.0
+                           Just (projection, _, _) -> len projection
+                           where
+                           snd3 (_, v, _)   = v
+                           initialDirection = translation (snd3 s1) &- translation (snd3 s2)
+                           notNullDirection = if lensqr initialDirection /= 0.0 then translation (snd3 s1)
+                                              else 1.0
+                           initialPoint     = sampleCSO s1 s2 $ mkNormal notNullDirection
+
+algorithmGJK :: (ImplicitShape g1 v, ImplicitShape g2 v, Transform m v av, UnitVector v n, Eq v) =>
+                (g1, m, m) -> (g2, m, m) -> Int -> SimplexResult v -> Maybe (v, Simplex v, [ Double ])
+algorithmGJK s1 s2 dimension (Left (projection, barCoords, lastSimplex, newSimplex))
+             | sDim /= 0 && (sDim == dimension + 1 || lensqr projection <= epsTol * maxLen newSimplex) = Nothing
+             | otherwise = algorithmGJK s1 s2 dimension $ stepGJK s1 s2 lastSimplex newSimplex projection barCoords
+             where
+             sDim = S.dimension newSimplex
+algorithmGJK _ _ _ (Right (projection, barCoords, simplex)) = 
+             Just (projection, simplex, barCoords)
+
+stepGJK :: (ImplicitShape g1 v, ImplicitShape g2 v, Transform m v av, UnitVector v n, Eq v) =>
+           (g1, m, m) -> (g2, m, m) -> Simplex v -> Simplex v -> v -> [ Double ] -> SimplexResult v
+stepGJK s1 s2 lastSimplex newSimplex v barCoords =
+        if contains csoPoint lastSimplex || sqlenv - v &. csoPoint <= sqEpsRel * sqlenv then
+          Right $ (v, barCoords, newSimplex) -- terminate the algorithm: distance has acceptable precision
+        else
+          Left $ catTuple4 newSimplex' (projectOrigin newSimplex')
+        where
+        sqlenv                 = lensqr v
+        oppnv                  = mkNormal $ neg v
+        csoPoint               = sampleCSO s1 s2 oppnv
+        newSimplex'            = addPoint csoPoint newSimplex
+        catTuple4 c (a, b, d)  = (a, b, c, d)
+
+sampleCSO :: (ImplicitShape g1 v, ImplicitShape g2 v, Transform m v av, UnitVector v n) =>
+             (g1, m, m) -> (g2, m, m) -> n -> v
+sampleCSO (g1, t1, it1) (g2, t2, it2) dir = p1 &- p2
+                                            where
+                                            p1     = supportPointWithTransform g1 dir    t1 it1
+                                            p2     = supportPointWithTransform g2 oppDir t2 it2
+                                            oppDir = toNormalUnsafe $ neg $ fromNormal dir
+
+sqEpsRel :: Double
+sqEpsRel = sqrt epsTol -- FIXME
+
+epsTol :: Double
+epsTol = 100.0 * (2.0 ** (-53)) -- FIXME
---
+++ Physics/Falling/Collision/Detection/Simplex.hs
@@ -0,0 +1,85 @@
+module Physics.Falling.Collision.Detection.Simplex
+(
+Simplex
+, emptySimplex
+, dimension
+, contains
+, addPoint
+, maxLen
+, projectOrigin
+)
+where
+
+import Data.Vect.Double.Base
+
+data (DotProd v, Vector v) => Simplex v = Simplex [ v ]
+
+emptySimplex :: (DotProd v, Vector v) => Simplex v
+emptySimplex = Simplex []
+
+dimension :: (DotProd v, Vector v) => Simplex v -> Int
+dimension (Simplex vs) = length vs
+
+contains :: (Eq v, DotProd v, Vector v) => v -> Simplex v -> Bool
+contains v (Simplex vs) = any (== v) vs
+
+addPoint :: (DotProd v, Vector v) => v -> Simplex v -> Simplex v
+addPoint v (Simplex vs) = Simplex (v:vs) -- FIXME: check degeneracies?
+
+maxLen :: (DotProd v, Vector v) => Simplex v -> Double
+maxLen (Simplex vs) = maximum $ map lensqr vs
+
+projectOrigin :: (DotProd v, Vector v) => Simplex v -> (v, [ Double ], Simplex v)
+projectOrigin (Simplex [])       = error "Cannot project the origin on an empty simplex."
+projectOrigin s@(Simplex (v:[])) = (v , [ 1.0 ], s)
+projectOrigin (Simplex (pts))    = (projection, barycentricCoordinates, Simplex bestCofactorVector)
+                                   where
+                                   cofs       = [ (uncurry cofactor) (extract i pts) lastPtsId
+                                                  | i <- [ 0 .. lastPtsId] ]
+                                   cofsValues = map fst4 cofs
+                                   lastPtsId  = length pts - 1
+                                   (_, bestCofactorValues, bestCofactorVector, _) =
+                                     if cofsValues == [] then
+                                       error "Empty cofs."
+                                     else if all (> 0.0) $ cofsValues then
+                                       (undefined, map fst4 cofs, pts, undefined)
+                                     else
+                                       foldr1 (\o@(_, _, _, depth) b@(_, _, _, best) ->
+                                               if depth > best then o else b)
+                                              cofs
+                                   bestDeterminant        = (sum bestCofactorValues)
+                                   barycentricCoordinates = map (/ bestDeterminant) bestCofactorValues
+                                   projection = foldr1 (&+)
+                                                $ zipWith (*&) barycentricCoordinates bestCofactorVector
+
+cofactor :: (DotProd v, Vector v) => v -> [ v ] -> Int -> (Double, [ Double ], [ v ], Int)
+cofactor _ [] _ = error "Cannot compute the cofactor of an empty matrix."
+
+cofactor rm l@(v:[]) _ = if cofactorValue > 0.0 then
+                           (cofactorValue, [ cofactorValue ], l, 1)
+                         else
+                           (cofactorValue, [], [], 0)
+                         where
+                         cofactorValue = (v &- rm) &. v
+cofactor rm vs lvs     = if all (> 0.0) subCofactorValues then
+                           (cofactorValue, subCofactorValues, vs, lvs)
+                         else
+                           (cofactorValue, bestCofactorValues, bestCofactorVector, bestCofactorLength)
+                         where
+                         p1                = vs !! 0
+                         rmp1              = p1 &- rm
+                         subFactorLength   = lvs - 1
+                         subCofactors      = [ (uncurry cofactor) (extract i vs) subFactorLength
+                                               | i <- [ 0 .. subFactorLength] ]
+                         subCofactorValues = map fst4 subCofactors
+                         cofactorValue     = sum $ zipWith (\c v -> c * (rmp1 &. v)) subCofactorValues vs
+                         (_, bestCofactorValues, bestCofactorVector, bestCofactorLength) =
+                           foldr1 (\o@(_, cofs, _, depth) b@(_, _, _, best) ->
+                                   if cofs /= [] && depth > best then b else o)
+                                  subCofactors
+
+fst4 :: (a, b, c, d) -> a
+fst4 (a, _, _, _) = a
+
+extract :: Int -> [a] -> (a, [a])
+extract i l = let (sub1, e:sub2) = splitAt i l in (e, sub1 ++ sub2)
