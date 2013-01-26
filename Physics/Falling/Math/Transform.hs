dca3f716fbb8b16ea156e5359c1d3944cac11de3
Sébastien Crozet <developer@crozet.re>
2013-01-26 13:26:23 +0100

    Removed meaningless fundeps.

--- Physics/Falling/Math/Transform.hs
+++ Physics/Falling/Math/Transform.hs
@@ -16,21 +16,20 @@ import Data.Vect.Double.Base hiding(translation)
 
 -- FIXME: rename this module with something more descriptive
 
-class DeltaTransform p v | p -> v, v -> p where
+class DeltaTransform p v where
   deltaTransform :: p -> v -> v
 
-class Translation p v | p -> v, v -> p where
+class Translation p v | p -> v where
   translation  :: p -> v
   translate    :: v -> p -> p
 
-class Rotation p r | p -> r, r -> p where
+class Rotation p r | p -> r where
   -- rotation :: p -> r
   rotate   :: r -> p -> p
 
 class (MultSemiGroup p, Matrix p, DeltaTransform p v, Rotation p a, Translation p v,
        PerpProd v a, DotProd v, DotProd a) =>
-      Transform p v a | p -> v
-                        , p -> a where
+      Transform p v a | p -> a where
   transform :: p -> v -> v
   transform proj vect = deltaTransform proj vect &+ translation proj
 
