7829158ace686a7dfb127a39eb91822bd74981d3
Sébastien Crozet <developer@crozet.re>
2013-02-24 18:37:04 +0000

    Allowed negative penetration depth on the constraint solver.

--- Physics/Falling/Constraint/Solver/SecondOrderWorldAccumulatedImpulse.hs
+++ Physics/Falling/Constraint/Solver/SecondOrderWorldAccumulatedImpulse.hs
@@ -18,7 +18,7 @@ import Physics.Falling.Constraint.Solver.AccumulatedImpulseSystem
 
 
 secondOrderErrorEstimator :: Double -> Double
-secondOrderErrorEstimator penDepth = max 0.0 $ (min gap penDepth) - 0.01
+secondOrderErrorEstimator penDepth = (min gap penDepth) - 0.01
 
 addSecondOrderEquations :: (OrthonormalBasis lv n, Dynamic rb t lv av ii, UnitVector lv n) =>
                            Double ->
@@ -45,12 +45,11 @@ addSecondOrderEquation dt bodies shift (UnibodyCollision idx coll _) sys =
                        confs  = (0.0, infinity, err, secondOrderWorldContact dt rb center normal False)
                                 : map (\a -> (-0.7 * 9.81, 0.7 * 9.81, 0.0, a))
                                       (map (\t -> secondOrderWorldContact dt rb center t False) tangeants)
-                       depth  = penetrationDepth coll
-                       err    = secondOrderErrorEstimator depth
-                       normal = contactNormal coll
-                       center = contactCenter coll
+                       err       = secondOrderErrorEstimator $ penetrationDepth coll
+                       normal    = contactNormal coll
+                       center    = contactCenter coll
                        tangeants = snd $ completeBasis normal
-addSecondOrderEquation dt bodies shift (BibodyCollision  id1 id2 coll _) sys =
+addSecondOrderEquation dt bodies shift (BibodyCollision id1 id2 coll _) sys =
                        foldr (\(ll, ul, e, conf1, conf2) s -> addTwoBodiesEquation conf1 conf2 sid1 sid2 ll ul (e / dt) s) sys confs -- FIXME: uggly to set limits and depth here
                        where
                        sid1      = id1 + shift
@@ -59,10 +58,9 @@ addSecondOrderEquation dt bodies shift (BibodyCollision  id1 id2 coll _) sys =
                        rb2       = bodies V.! sid2
                        confs     = (0.0, infinity, err, secondOrderWorldContact dt rb1 center normal True, secondOrderWorldContact dt rb2 center normal False)
                                    : map (\(a, b) -> (-0.7 * 9.81, 0.7 * 9.81, 0.0, a, b))
-                                         (zip (map (\t -> secondOrderWorldContact dt rb1 center t True) tangeants)
+                                         (zip (map (\t -> secondOrderWorldContact dt rb1 center t True)  tangeants)
                                               (map (\t -> secondOrderWorldContact dt rb2 center t False) tangeants))
-                       depth     = penetrationDepth coll
-                       err       = secondOrderErrorEstimator depth
+                       err       = secondOrderErrorEstimator $ penetrationDepth coll
                        normal    = contactNormal coll
                        center    = contactCenter coll
                        tangeants = snd $ completeBasis normal
@@ -87,8 +85,8 @@ secondOrderIntegrate dt bodies linImpVect angImpVect shift =
             let idx    = i + shift in
             let linImp = linImpVect UV.! idx in
             let angImp = angImpVect UV.! idx in
-            let rb'    = setVelocities (linearVelocity  rb &+ externalLinearForce  rb &* dt,
-                                        angularVelocity rb &+ externalAngularForce rb &* dt)
+            let rb'    = setVelocities (linearVelocity  rb &+ dt *& externalLinearForce  rb,
+                                        angularVelocity rb &+ dt *& externalAngularForce rb)
                                        rb
             in
             (i, applyImpulses linImp angImp rb')
